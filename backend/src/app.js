45943004ead3d94b18516273b67b0514c71b4665
Abhi Patel <abhijpatel1234.ap@gmail.com>
2026-05-24 11:55:01 -0500

    fix cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:3000" || "https://foo-reel.vercel.app/",
+    origin: ["http://localhost:3000", "http://localhost:5173", "https://foo-reel.vercel.app"],
     credentials: true
 }));
 app.use(cookieParser());
